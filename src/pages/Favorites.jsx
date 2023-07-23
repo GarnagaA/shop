c9b9d71490afb4bbb8bfe9bc67063d57bf4c935d
Garnaga <rostr9211@gmail.com>
2023-07-23 23:43:07 +0300

    chenge pc

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,9 +1,30 @@
+import Search from "../components/Search";
+import Card from "../components/Card";
 
+export function Home( props ) {
 
-export function Favorites () {
-
-
+const {setSearchValue,
+    searchValue,
+    items,
+    onAddToFavorites,
+    onAddToBasket,
+} = props
     return (
-        <h1>Favorites page</h1>
-    )
+        <div className="content2 p-40">
+            <Search setSearchValue={setSearchValue} searchValue={searchValue}/>
+            <ul className='d-flex flex-wrap'>{items
+                .filter(obj => obj.title.toLowerCase().includes(searchValue.toLowerCase()))
+                .map( (item, index) =>
+                    <Card
+                        {...item} // сокращённая запись
+                        key={index}
+                        // onRemoveFromBasket={ () => onRemoveFromBasket(item.id)}
+                        // onRemoveFromFavorites={() => onRemoveFromFavorites(item.id)}
+                        onAddToFavorites={() => onAddToFavorites(item)}
+                        onAddToBasket={() => onAddToBasket(item)}
+                        // favorites={favorites}
+                        // setFavorites={setFavorites}
+                    />)}
+            </ul>
+        </div>    )
 }
