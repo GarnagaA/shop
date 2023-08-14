edf046ebabbec450920406ebb3c8f39ef24fb812
Andrei G <garnaga.and@gmail.com>
2023-08-14 18:42:44 +0300

    deploy version

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-		< HashRouter basename={process.env.PUBLIC_URL}>
+
+		<HashRouter basename={process.env.PUBLIC_URL}>
 				<App />
 		</HashRouter>
 )
